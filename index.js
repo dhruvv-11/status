a192af24dfa80357da6dad6cf675d061d44527c1
peace10-dhruv <83553200+peace10-dhruv@users.noreply.github.com>
2021-06-02 00:27:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //do install the following packages. npm i discord.js and npm i discord-rpc
+// in your discord api portal go to rich presence then upload a image of size of 512 x 512. now rename the following image as large or small. if you renamed one of the image large 
+//then the large image size would be big in your status and if u have renamed your image to small then in your status the image would be in small size. 
+// you can edit the details, state, the discord buttons, the large image key text and the small image key tet. 
+//if any problem you can create an issuse or you can contact me on Discord - 𝐂𝐑𝐍〢ζ͜͡ᗪᕼᖇᑌᐯ乛#1104. my server link - https://discord.gg/SUGX54SZKx
+//hope it helps
 const RPC = require('discord-rpc')
 const rpc = new RPC.Client({
     transport: 'ipc'
@@ -16,7 +21,7 @@ rpc.on("ready", () => {
         buttons: [{label : "Discord Link <3", url : "https://discord.gg/Z6vXdaBXJn"}]
     })
 
-    console.log("STATUS_MARK-1 SUCCESSFUL")
+    console.log("STATUS SUCCESSFULL")
 })
 
 rpc.login({
